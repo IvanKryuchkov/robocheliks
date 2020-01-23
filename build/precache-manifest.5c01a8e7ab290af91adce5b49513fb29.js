616918b9ddf77031e613c06a07e7c0620da6edb1
IvanKryuchkov <55559380+IvanKryuchkov@users.noreply.github.com>
2020-01-24 00:41:22 +0200

    based commit

---
+++ build/precache-manifest.5c01a8e7ab290af91adce5b49513fb29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853ce7fad7a58abaa8628223aefe0569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4a32912ceb0fbcb566dc",
+    "url": "/static/css/main.37b447b2.chunk.css"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/static/js/2.37296ba2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.37296ba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a32912ceb0fbcb566dc",
+    "url": "/static/js/main.6a1a7206.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  }
+]);
